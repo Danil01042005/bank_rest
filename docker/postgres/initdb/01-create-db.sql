444c056b1109abcf40646ee4909d275f1c46c22c
Danil01042005 <skotdaivanov@gmail.com>
2025-11-04 16:30:54 +0300

    Update Maven wrapper, add initial database setup script, and configure database migration for user and card tables

---
+++ docker/postgres/initdb/01-create-db.sql
@@ -0,0 +1,12 @@
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'bankdb'
+  ) THEN
+    CREATE DATABASE bankdb;
+    ALTER DATABASE bankdb OWNER TO bankuser;
+  END IF;
+END$$;
+
+
+
